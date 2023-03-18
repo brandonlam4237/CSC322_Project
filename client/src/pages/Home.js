58b6726ea2388f0027be4e36435551d7b1122b2f
Brandon Lam <brandonlam4237@gmail.com>
2023-03-18 14:41:54 -0400

    home page layout

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,28 @@
 import Navbar from "../components/Navbar";
+import "../scss/home.scss";
+import pcCase from "../assets/images/pc.png";
+import comp from "../assets/images/3090.png";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div>
-      <Navbar /> home
-    </div>
+    <main className="home">
+      <Navbar />
+      <section className="container">
+        <div className="container__area-1">
+          <Link to="/builds" className="container__link-1">
+            Featured Builds
+          </Link>
+          <img src={pcCase} className="container__img-1" alt="pc" />
+        </div>
+        <div className="container__area-2">
+          <Link to="/components" className="container__link-2">
+            Browse Components
+          </Link>
+          <img src={comp} className="container__img-2" alt="3090" />
+        </div>
+      </section>
+    </main>
   );
 }
 
