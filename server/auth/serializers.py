9a42ec9a6ac77b4525ea4ede0f40255d50548639
Jed Rendo Magracia <jedmagracia@gmail.com>
2023-04-08 13:37:02 -0400

    feat: Added UserAccount serializers in auth

---
+++ server/auth/serializers.py
@@ -0,0 +1,87 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer for general user type
+    """
+    class Meta:
+        """
+        Fields
+        ------
+        model : django Model
+            User model
+        fields : tuple[str]
+            Tuple of every field to be serialized
+        """
+        model = User
+        fields = (
+            'id',
+            'username',
+            'email',
+            'is_active',
+            'first_name',
+            'last_name',
+            'date_created',
+            'is_customer',
+            'is_employee',
+            'blacklisted',
+            'balance',
+            'memo',
+        )
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Employee user type
+    """
+    class Meta:
+        """
+        Fields
+        ------
+        model : django Model
+            User model
+        fields : tuple[str]
+            Tuple of every field to be serialized
+        """
+        model = User
+        fields = (
+            'id',
+            'username',
+            'email',
+            'is_active',
+            'first_name',
+            'last_name',
+            'blacklisted',
+            'memo',
+        )
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Customer user type
+    """
+    class Meta:
+        """
+        Fields
+        ------
+        model : django Model
+            User model
+        fields : tuple[str]
+            Tuple of every field to be serialized
+        """
+        model = User
+        fields = (
+            'id',
+            'username',
+            'email',
+            'is_active',
+            'first_name',
+            'last_name',
+            'balance',
+            'blacklisted',
+            'memo',
+        )
