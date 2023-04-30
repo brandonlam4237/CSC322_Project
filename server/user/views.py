09c638d5f7d4000762f426ea3e80e4e169fa97b2
Jed Rendo Magracia <jedmagracia@gmail.com>
2023-04-30 14:04:21 -0400

    feat: Added view/endpoint for adding balance

--- server/user/views.py
+++ server/user/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.views import APIView
@@ -185,6 +186,146 @@ class CustomerDetail(APIView):
                 {'error': 'Something went wrong when retrieving customer data'},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
             )
+
+
+class AddBalance(APIView):
+    """
+    An API View class for adding balance.
+
+    Permissions
+    -----------
+        User must be a Customer type
+
+    Methods
+    -------
+    patch(request)
+        Handles a PATCH request to update Customer balance
+    """
+
+    def validate_credit_card(self, card_number: str) -> bool:
+        """This function validates a credit card number."""
+        try:
+            # 1. Change datatype to list[int]
+            card_number = [int(num) for num in card_number]
+
+            # 2. Remove the last digit:
+            check_digit = card_number.pop(-1)
+
+            # 3. Reverse the remaining digits:
+            card_number.reverse()
+
+            # 4. Double digits at even indices
+            card_number = [num * 2 if idx % 2 == 0
+                           else num for idx, num in enumerate(card_number)]
+
+            # 5. Subtract 9 at even indices if digit is over 9
+            # (or you can add the digits)
+            card_number = [num - 9 if idx % 2 == 0 and num > 9
+                           else num for idx, num in enumerate(card_number)]
+
+            # 6. Add the checkDigit back to the list:
+            card_number.append(check_digit)
+
+            # 7. Sum all digits:
+            check_sum = sum(card_number)
+
+            # 8. If checkSum is divisible by 10, it is valid.
+            return check_sum % 10 == 0
+        except Exception:
+            return False
+
+    def validate_exp_date(self, exp_date: str) -> bool:
+        """This function validates a credit card expiration date."""
+        try:
+            if len(exp_date) != 6:
+                return False
+
+            if int(exp_date[:2]) > 12:
+                return False
+
+            int(exp_date[2:])
+
+            return True
+        except Exception:
+            return False
+
+    def validate_balance(self, balance: float) -> bool:
+        """This function validates the balance input."""
+        if balance <= 0 or balance > 15000:
+            return False
+        return True
+
+    def is_expired(self, exp_date: str) -> bool:
+        """This function checks if the card is expired."""
+        exp_month = int(exp_date[:2])
+        exp_year = int(exp_date[2:])
+
+        today = date.today()
+        exp_date = date(year=exp_year, month=exp_month, day=1)
+
+        return today > exp_date
+
+    def patch(self, request):
+        """Handles a PATCH request to update Customer balance"""
+        user = request.user
+
+        if not user.is_customer:
+            return Response(
+                {'error': 'User must be a customer to use this endpoint'},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        credit_card_num = request.data.get('card_number')
+        exp_date = request.data.get('exp_date')
+        balance = request.data.get('balance')
+
+        if not credit_card_num:
+            return Response(
+                {'error': 'Please provide credit card number'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if not exp_date:
+            return Response(
+                {'error': 'Please provide expiration date'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if not balance:
+            return Response(
+                {'error': 'Please provide balance to add'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if not self.validate_credit_card(credit_card_num):
+            return Response(
+                {'error': 'Invalid credit card'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if not self.validate_exp_date(exp_date):
+            return Response(
+                {'error': 'Invalid expiration date'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if self.is_expired(exp_date):
+            return Response(
+                {'error': 'Credit card is expired'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if not self.validate_balance(balance):
+            return Response(
+                {'error': 'Balance must be over 0 and less than or equal to 15000'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        User.objects.filter(id=user.id).update(balance=user.balance+balance)
+        return Response(
+            {'success': 'User balance has been updated'},
+            status=status.HTTP_200_OK
+        )
 
 
 class BlacklistedUserList(APIView):
