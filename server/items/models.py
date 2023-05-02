99727a2eafd28fea00046488d8cf41e35a89a3b3
Jed Rendo Magracia <jedmagracia@gmail.com>
2023-05-01 23:14:20 -0400

    feat: Added inheritance to item models

--- server/items/models.py
+++ server/items/models.py
@@ -43,7 +43,29 @@ class Product(models.Model):
     product_name = models.CharField(max_length=255, null=False)
     price = models.DecimalField(max_digits=7, decimal_places=2, null=False)
     comments = models.ManyToManyField(Comment, blank=True)
-    image_url = models.URLField(max_length=200, null=False)
+
+    objects = models.Manager()
+
+    def __str__(self) -> str:
+        return f"{self.brand} - {self.product_name}"
+
+
+class ComputerPart(Product):
+    """
+    ComputerPart Model
+
+    Fields
+    ------
+    category : CharField
+        Category of part
+    image_url: URLField
+        Image URL of part
+    product_sku: IntegerField
+    manufacturer_part_number: CharField
+    upc_number: CharField
+    specs: JSONField
+        Specifications of computer part
+    """
 
     class Category(models.TextChoices):
         """
@@ -62,18 +84,14 @@ class Product(models.Model):
     category = models.CharField(
         max_length=20, choices=Category.choices, null=False)
 
+    image_url = models.URLField(max_length=200, null=False)
     product_sku = models.IntegerField(null=False)
     manufacturer_part_number = models.CharField(max_length=100, null=False)
     upc_number = models.CharField(max_length=20, null=False)
     specs = models.JSONField(null=False)
 
-    objects = models.Manager()
 
-    def __str__(self) -> str:
-        return f"{self.brand} - {self.product_name}"
-
-
-class CustomBuild(models.Model):
+class CustomBuild(Product):
     """
     Custom Build Model
 
@@ -87,10 +105,12 @@ class CustomBuild(models.Model):
         Parts the Custom Build Takes
     """
     build_maker = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
-    build_name = models.CharField(max_length=50, unique=True, null=False)
-    parts = models.ManyToManyField(Product)
+    parts = models.ManyToManyField(ComputerPart, related_name="build_parts")
 
     objects = models.Manager()
+
+    def __str__(self) -> str:
+        return f"{self.build_maker.username} - {self.product_name}"
 
 
 class Order(models.Model):
@@ -111,7 +131,7 @@ class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
 
     address = models.TextField(null=False)
-    items = models.ManyToManyField(Product, related_name='order_list')
+    items = models.ManyToManyField(Product, related_name='order_items')
 
     datetime_ordred = models.DateTimeField(auto_now_add=True, null=False)
     total_price = models.DecimalField(
@@ -120,7 +140,7 @@ class Order(models.Model):
     objects = models.Manager()
 
 
-class ShoppingCart(models.Model):
+class Cart(models.Model):
     """
     Shopping Cart Model
 
@@ -128,11 +148,37 @@ class ShoppingCart(models.Model):
     ------
     customer : OneToOneField
         Customer who owns the shopping cart
-    items : ManyToManyField
-        Items within the shopping cart
     """
     customer = models.OneToOneField(User, on_delete=models.CASCADE, null=False)
-    items = models.ManyToManyField(
-        Product, related_name='items', blank=True)
 
     objects = models.Manager()
+
+    @property
+    def total_price(self):
+        cartitems = self.cart_items.all()
+        total = sum([item.price for item in cartitems])
+        return total
+
+    @property
+    def num_items(self):
+        cartitems = self.cart_items.all()
+        quantity = sum([item.quantity for item in cartitems])
+        return quantity
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='items')
+    cart = models.ForeignKey(
+        Cart, on_delete=models.CASCADE, related_name='cart_items')
+    quantity = models.IntegerField(default=0)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.product.product_name
+
+    @property
+    def price(self):
+        new_price = self.product.price * self.quantity
+        return new_price
