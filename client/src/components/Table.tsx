a5a35807ad9d30f2669004eece624cfb0a7c8517
ShehabMohsen <aquashehab612@gmail.com>
2023-05-03 17:18:46 -0400

    feat: set up logic for conditional rendering of parts based on selection

--- client/src/components/Table.tsx
+++ client/src/components/Table.tsx
@@ -1,84 +1,111 @@
 import "../scss/table.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faXmark, faPlus } from "@fortawesome/free-solid-svg-icons";
+import Button from "./Button";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const PartsPathArray: string[] = [
+  "cpus",
+  "coolers",
+  "motherboards",
+  "memory",
+  "storage",
+  "videocards",
+  "power",
+  "cases",
+];
+
+const componentNames: string[] = [
+  "CPU",
+  "CPU Cooler",
+  "Motherboard",
+  "Memory",
+  "Storage",
+  "Video Card",
+  "Power Supply",
+  "Case",
+];
+
+interface Part {
+  component_name: string;
+  component_selection: string;
+  img_url: string;
+  price: number;
+}
+
+interface PartsList {
+  CPU: Part;
+  "CPU Cooler": Part;
+  Motherboard: Part;
+  Memory: Part;
+  Storage: Part;
+  "Video Card": Part;
+  "Power Supply": Part;
+  Case: Part;
+}
+
 export default function Table() {
+  const [partsList, setPartsList] = useState<any>({
+    "CPU Cooler": {
+      component_name: "CPU Cooler",
+      component_selection: "Corsair RGB Pro Cooler",
+      img_url: "url",
+      price: 159,
+    },
+  });
+
   return (
-      <table>
-        <thead>
-          <tr>
-            <th>Part</th>
-            <th>Selection</th>
-            <th>Price</th>
-            <th></th>
-          </tr>
-        </thead>
-        <br />
-        <tbody>
-          <tr>
-            <th>Motherboard</th>
-            <td>Asus ROG B550</td>
-            <td>$169.99</td>
-            <td className="remove-icon">
-              <FontAwesomeIcon icon={faXmark} size="xl" />
-            </td>
-          </tr>
-          <tr>
-            <th>CPU</th>
-            <td>AMD Ryzen 5600x</td>
-            <td>$349.99</td>
-            <td className="remove-icon">
-              <FontAwesomeIcon icon={faXmark} size="xl" />
-            </td>
-          </tr>
-          <tr>
-            <th>GPU</th>
-            <td>MSI Ventus 3x 3070</td>
-            <td>$699.99</td>
-            <td className="remove-icon">
-              <FontAwesomeIcon icon={faXmark} size="xl" />
-            </td>
-          </tr>
-          <tr>
-            <th>Case</th>
-            <td>NZXT H510 Elite</td>
-            <td>$149.99</td>
-            <td className="remove-icon">
-              <FontAwesomeIcon icon={faXmark} size="xl" />
-            </td>
-          </tr>
-          <tr>
-            <th>Memory</th>
-            <td>component_name</td>
-            <td>price</td>
-            <td className="remove-icon">
-              <FontAwesomeIcon icon={faXmark} size="xl" />
-            </td>
-          </tr>
-          <tr>
-            <th>CPU Cooler</th>
-            <td>component_name</td>
-            <td>price</td>
-            <td className="remove-icon">
-              <FontAwesomeIcon icon={faXmark} size="xl" />
-            </td>
-          </tr>
-          <tr>
-            <th>Storage</th>
-            <td>component_name</td>
-            <td>price</td>
-            <td className="remove-icon">
-              <FontAwesomeIcon icon={faXmark} size="xl" />
-            </td>
-          </tr>
-          <tr>
-            <th>Power Supply Unit</th>
-            <td>component_name</td>
-            <td>price</td>
-            <td className="remove-icon">
-              <FontAwesomeIcon icon={faXmark} size="xl" />
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <table>
+      <thead>
+        <tr>
+          <th>Part</th>
+          <th>Selection</th>
+          <th>Price</th>
+          <th></th>
+        </tr>
+      </thead>
+      <br />
+      <tbody>
+        <TableBody partsList={partsList} />
+      </tbody>
+    </table>
+  );
+}
+
+function TableBody({ partsList }: any) {
+  return (
+    <>
+      {componentNames.map((componentName, index) => {
+        if (partsList[`${componentName}`]) {
+          return (
+            <tr>
+              <th>{componentName}</th>
+              <td>AMD Ryzen 5600x</td>
+              <td>$349.99</td>
+              <td className="remove-icon">
+                <FontAwesomeIcon icon={faXmark} size="xl" />
+              </td>
+            </tr>
+          );
+        } else {
+          return (
+            <tr>
+              <th>{componentName}</th>
+              <td>
+                <Link to={`/components/${PartsPathArray[index]}`}>
+                  <Button className="blue-primary">
+                    <FontAwesomeIcon icon={faPlus} size="lg" /> Choose a{" "}
+                    {componentName}
+                  </Button>
+                </Link>
+              </td>
+              <td></td>
+              <td></td>
+            </tr>
+          );
+        }
+      })}
+    </>
   );
 }
