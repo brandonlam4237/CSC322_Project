15c1b005ef13ef714a0581e4333a97bb38dc5352
ShehabMohsen <aquashehab612@gmail.com>
2023-05-06 16:15:21 -0400

    feat: add part to parts list from product card and product detail

--- client/src/components/PartsTable.tsx
+++ client/src/components/PartsTable.tsx
@@ -2,10 +2,14 @@ import "../scss/table.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark, faPlus } from "@fortawesome/free-solid-svg-icons";
 import Button from "./Button";
-import { useState } from "react";
 import { Link } from "react-router-dom";
+import {
+  usePartsListContext,
+  IPartsList,
+  IPart,
+} from "src/contexts/PartsListContext";
 
-const PartsPathArray: string[] = [
+const partsCategoryArray: string[] = [
   "CPU",
   "Cooling",
   "Motherboard",
@@ -16,74 +20,25 @@ const PartsPathArray: string[] = [
   "Case",
 ];
 
-const componentNames: string[] = [
-  "CPU",
-  "CPU Cooler",
-  "Motherboard",
-  "Memory",
-  "Storage",
-  "Video Card",
-  "Power Supply",
-  "Case",
-];
-
-interface IPart {
-  component_name: string;
-  product_name: string;
-  image_url: string;
-  price: number;
-  isAdded: Boolean;
+interface IRowHeader {
+  [key: string]: string;
 }
-
-interface IPartsList {
-  /* enclosing CPU and denoting its type as string prevents implicit type
-  any on keys error when using a string to access as a key to access the object */
-  [CPU: string]: IPart;
-  "CPU Cooler": IPart;
-  Motherboard: IPart;
-  Memory: IPart;
-  Storage: IPart;
-  "Video Card": IPart;
-  "Power Supply": IPart;
-  Case: IPart;
-}
-
-const partTemplate: IPart = {
-  component_name: "",
-  product_name: "",
-  image_url: "",
-  price: -1,
-  isAdded: false,
+const rowHeader: IRowHeader = {
+  CPU: "CPU",
+  Cooling: "CPU Cooler",
+  Motherboard: "Motherboard",
+  RAM: "Memory",
+  Storage: "Storage",
+  GPU: "Video Card",
+  PSU: "Power Supply",
+  Case: "Case",
 };
 
 export default function PartsTable() {
-  const [partsList, setPartsList] = useState<IPartsList>({
-    CPU: partTemplate,
-    "CPU Cooler": {
-      component_name: "CPU Cooler",
-      product_name:
-        "iCUE H100i ELITE CAPELLIX XT 240mm All in One Liquid CPU Cooling Kit",
-      image_url:
-        "https://www.microcenter.com/endeca/zoomFullScreen.aspx?src=662683_537092_01_package_comping-jpg",
-      price: 159.99,
-      isAdded: true,
-    },
-    Motherboard: partTemplate,
-    Memory: partTemplate,
-    Storage: partTemplate,
-    "Video Card": {
-      component_name: "Video Card",
-      product_name:
-        "NVIDIA GeForce RTX 4080 AERO Overclocked Triple Fan 16GB GDDR6X PCIe 4.0 Graphics Card",
-      image_url:
-        "https://90a1c75758623581b3f8-5c119c3de181c9857fcb2784776b17ef.ssl.cf2.rackcdn.com/660700_520452_01_front_zoom.jpg",
-      price: 1199.99,
-      isAdded: true,
-    },
-    "Power Supply": partTemplate,
-    Case: partTemplate,
-  });
-
+  const partsListVariables = usePartsListContext();
+  const partsList = partsListVariables.partsList;
+  const setPartsList = partsListVariables.setPartsList;
+  const removePart = partsListVariables.removePart;
   return (
     <table>
       <thead>
@@ -96,7 +51,11 @@ export default function PartsTable() {
       </thead>
       <br />
       <tbody>
-        <TableBody partsList={partsList} setPartsList={setPartsList} />
+        <TableBody
+          partsList={partsList}
+          setPartsList={setPartsList}
+          removePart={removePart}
+        />
       </tbody>
     </table>
   );
@@ -104,33 +63,19 @@ export default function PartsTable() {
 
 interface TableBodyProps {
   partsList: IPartsList;
-  setPartsList: (part: IPartsList) => void;
+  setPartsList: (value: IPartsList) => void;
+  removePart: (value: IPart) => void;
 }
-function TableBody({ partsList, setPartsList }: TableBodyProps) {
-  function removePart(part: IPart) {
-    const componentToRemove: string = part.component_name;
-    setPartsList({
-      ...partsList,
-      [componentToRemove]: partTemplate,
-    });
-  }
 
-  function addPart(part: IPart) {
-    const componentToAdd: string = part.component_name;
-    setPartsList({
-      ...partsList,
-      [componentToAdd]: part,
-    });
-  }
-
+function TableBody({ partsList, setPartsList, removePart }: TableBodyProps) {
   return (
     <>
-      {componentNames.map((componentName, index) => {
-        const part: any = partsList[`${componentName}`];
+      {partsCategoryArray.map((categoryName, index) => {
+        const part: IPart = partsList[categoryName];
         if (part.isAdded) {
           return (
             <tr>
-              <th className="row-header">{componentName}</th>
+              <th className="row-header">{rowHeader[categoryName]}</th>
               <td>{part.product_name}</td>
               <td>${part.price}</td>
               <td className="remove-icon">
@@ -145,12 +90,12 @@ function TableBody({ partsList, setPartsList }: TableBodyProps) {
         } else {
           return (
             <tr>
-              <th className="row-header">{componentName}</th>
+              <th className="row-header">{rowHeader[categoryName]}</th>
               <td>
-                <Link to={`/products/${PartsPathArray[index]}`}>
+                <Link to={`/products/${partsCategoryArray[index]}`}>
                   <Button className="blue-primary">
                     <FontAwesomeIcon icon={faPlus} size="lg" /> Choose a{" "}
-                    {componentName}
+                    {categoryName}
                   </Button>
                 </Link>
               </td>
--- client/src/pages/Products.tsx
+++ client/src/pages/Products.tsx
@@ -4,7 +4,7 @@ import "../scss/products.scss";
 import ProductCard from "src/components/ProductCard";
 
 function Prodcuts() {
-  const { id: category } = useParams();
+  const category = useParams().id || "";
   const [productList, setProductList] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
   async function fetchProducts() {
@@ -35,6 +35,7 @@ function Prodcuts() {
                 product_name={item.product_name}
                 image_url="https://90a1c75758623581b3f8-5c119c3de181c9857fcb2784776b17ef.ssl.cf2.rackcdn.com/660700_520452_01_front_zoom.jpg"
                 price={item.price}
+                category = {category}
                 id={item.id}
               />
             );
