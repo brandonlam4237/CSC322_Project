1c574ea2b4d7a2ee8a6be8e8c71c0e237b0527e7
Brandon Lam <brandonlam4237@gmail.com>
2023-05-05 20:11:59 -0400

    feat: product detail styled layout

--- client/src/pages/ProductDetail.jsx
+++ client/src/pages/ProductDetail.jsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "../scss/productDetails.scss";
+import Button from "src/components/Button";
+import comment from "../assets/icons/comment.png";
 
 const fields = {
-  Case: ["Case Type", "Color", "Max Motherboard Size"],
+  Case: ["Case Type", "Color", "Max Motherboard Size", "Height", "Width"],
   Cooling: [
     "Compatible Intel Sockets",
     "Compatible AMD Sockets",
     "Power Consumption",
     "Radiator Size",
     "Radiator Material",
-    "LED Color",
     "Fan Size",
     "Fan System",
     "Fan Bearing",
@@ -25,8 +26,34 @@ const fields = {
     "Core Clock Speed",
     "CUDA Processors",
   ],
-  Motherboard: ["Chipset", "AMD CPU Support", "Socket Type", "Form Factor"],
-  PSU: ["Wattage", "Form Factor", "Color", "Modular"],
+  Motherboard: [
+    "Chipset",
+    "AMD CPU Support",
+    "Socket Type",
+    "Form Factor",
+    "Width",
+  ],
+  PSU: ["Wattage", "Form Factor", "Color", "Modular", "Height", "Width"],
+  CPU: ["VR Ready", "Processor Type", "Cores", "CPU Core", "Memory Type"],
+  RAM: [
+    "Memory Type",
+    "Memory Speed",
+    "Memory Speed (MHz)",
+    "Memory Capacity",
+    "Memory Channels",
+    "Memory Profiles",
+    "Number of Modules",
+    "Voltage",
+    "Memory Heatsink",
+  ],
+
+  Storage: [
+    "Capacity",
+    "Read Speed",
+    "Write Speed",
+    "Interface",
+    "Form Factor",
+  ],
 };
 
 function ProductDetail() {
@@ -54,8 +81,12 @@ function ProductDetail() {
     const specValsArr = [];
 
     for (const key in specObj) {
+      if (specObj[key]) {
+        specValsArr.push(specObj[key]);
+      } else {
+        specValsArr.push("");
+      }
       specKeysArr.push(key);
-      specValsArr.push(specObj[key]);
     }
 
     console.log(specKeysArr);
@@ -70,17 +101,37 @@ function ProductDetail() {
   return (
     <main className="productDetails">
       {!loading && (
-        <div>
-          <div>{productDetails.price}</div>
-          <div>{productDetails.product_name}</div>
-          <div>{productDetails.brand}</div>
-          <img
-            className="productDetails__img"
-            src="https://90a1c75758623581b3f8-5c119c3de181c9857fcb2784776b17ef.ssl.cf2.rackcdn.com/660700_520452_01_front_zoom.jpg"
-          />
-          {specKeys.map((specKey, idx) => {
-            return <div key={idx}>{`${specKey} : ${specVals[idx]}`}</div>;
-          })}
+        <div className="productDetails__container">
+          <div className="productDetails__card-front">
+            <img
+              className="productDetails__img"
+              src="https://90a1c75758623581b3f8-5c119c3de181c9857fcb2784776b17ef.ssl.cf2.rackcdn.com/660700_520452_01_front_zoom.jpg"
+            />
+          </div>
+          <div className="productDetails__card-back">
+            <div>
+              <div className="productDetails__name">
+                {productDetails.product_name}
+              </div>
+              <div className="productDetails__price">
+                {"$" + productDetails.price}
+              </div>
+            </div>
+
+            <div className="productDetails__specs">
+              {specKeys.map((specKey, idx) => {
+                return <div key={idx}>{`${specKey} : ${specVals[idx]}`}</div>;
+              })}
+            </div>
+            <div className="productDetails__btns">
+              <Button className="blue-primary">Add to cart</Button>
+              <Button className="black-primary">Add to build</Button>
+            </div>
+            <div className="productDetails__comment">
+              <img className="productDetails__comment-icon" src={comment} />
+              <div>Leave a comment</div>
+            </div>
+          </div>
         </div>
       )}
     </main>
