dbdf4f13f373f7922e87f89e82c12a3c2a810557
Jed Rendo Magracia <jedmagracia@gmail.com>
2023-05-07 16:51:04 -0400

    feat: Added Check Compatibility to URL endpoints

--- server/items/urls.py
+++ server/items/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import PartList, PartDetail
 from .views import ManageComment
+from .views import CheckCompatibility
 
 urlpatterns = [
     # Part URLs
@@ -9,5 +10,8 @@ urlpatterns = [
     path('<int:id>', PartDetail.as_view()),
 
     # Comment URLs
-    path('comments/<int:id>', ManageComment.as_view())
+    path('comments/<int:id>', ManageComment.as_view()),
+
+    # Build URLs
+    path('builds/compatibility', CheckCompatibility.as_view())
 ]
