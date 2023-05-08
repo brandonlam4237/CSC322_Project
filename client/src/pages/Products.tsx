a915f9ddc72785ee5496f9bea905da9c7571d1c7
ShehabMohsen <aquashehab612@gmail.com>
2023-05-07 21:25:38 -0400

    fix: replace placeholder image url with fetched urls

--- client/src/pages/Products.tsx
+++ client/src/pages/Products.tsx
@@ -33,7 +33,7 @@ function Prodcuts() {
               <ProductCard
                 key={i}
                 product_name={item.product_name}
-                image_url="https://90a1c75758623581b3f8-5c119c3de181c9857fcb2784776b17ef.ssl.cf2.rackcdn.com/660700_520452_01_front_zoom.jpg"
+                image_url={item.image_url}
                 price={item.price}
                 category = {category}
                 id={item.id}
