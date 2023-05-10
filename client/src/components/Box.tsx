953bf1a8c820eb6c3e02ca540a72e3e3a4a1347f
ShehabMohsen <aquashehab612@gmail.com>
2023-05-10 14:58:08 -0400

    feat: create styled box component to work as banner

---
+++ client/src/components/Box.tsx
@@ -0,0 +1,35 @@
+import { ReactNode } from "react";
+import "../scss/Box.scss";
+
+interface BoxProps {
+  className?: string;
+  color?: "green" | "blue" | "red";
+  isBold?: boolean;
+  isRound?: boolean;
+  padding?: number;
+  children: ReactNode;
+}
+
+export default function Box({
+  className,
+  color = "blue",
+  isBold = false,
+  padding = 10,
+  isRound = false,
+  children,
+  ...remainderProps
+}: BoxProps) {
+  return (
+    <div
+      className={`Box ${className} ${color}`}
+      style={{
+        fontWeight: isBold ? "bold" : "",
+        borderRadius: isRound ? "5px" : "",
+        padding: `${padding}px`,
+      }}
+      {...remainderProps}
+    >
+      {children}
+    </div>
+  );
+}
