c78161aa2a658547b11c005481305c57761b5035
ShehabMohsen <aquashehab612@gmail.com>
2023-05-13 17:27:36 -0400

    feat: implement checkbox styling and logic

--- client/src/pages/PartPicker.tsx
+++ client/src/pages/PartPicker.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useAuthContext } from "src/contexts/AuthContext";
 import "../scss/partpicker.scss";
+import "../scss/checkbox.scss";
 import PartsTable from "src/components/PartsTable";
 import Button from "src/components/Button";
 import { usePartsListContext } from "src/contexts/PartsListContext";
@@ -12,6 +13,7 @@ export default function MyBuild() {
   const user = authValues.userData;
   const [isLoading, setisLoading] = useState<boolean>(true);
   const [compatibilityIssues, setCompatibilityIssues] = useState([]);
+  const [isChecked, setIsChecked] = useState(false);
 
   // partsList context vairables
   const partsListVariables = usePartsListContext();
@@ -31,8 +33,28 @@ export default function MyBuild() {
     });
   }
 
+  function handleCheckBox() {
+    if (isChecked) {
+      setBuildForm({
+        ...buildForm,
+        build_name: "place holder name",
+        build_description: "place holder description",
+      });
+    } else {
+      setBuildForm({
+        ...buildForm,
+        build_name: "",
+        build_description: "",
+      });
+    }
+    setIsChecked(!isChecked);
+  }
+
   async function handleSuggestBuild() {
     let buildIdObject = await apiClient.createBuild(buildForm);
+    // if the user happens to be also customer then buy the build
+    if (user.user_type == "Customer")
+      await apiClient.checkoutBuild(buildIdObject.build_id);
     await apiClient.setBuildVisible(buildIdObject.build_id);
   }
 
@@ -51,7 +73,6 @@ export default function MyBuild() {
     }
     validateBuild();
   }, [partsListIds]);
-
   return (
     <div className="fullscreen-bg">
       {isLoading ? (
@@ -69,38 +90,51 @@ export default function MyBuild() {
               partsListIds={partsListIds}
             />
             <PartsTable />
-            {user.user_type == ("Owner" || "Employee") ? (
-              <div className="build-description-container">
-                <form>
-                  <h3>
-                    <label htmlFor="suggested-build-name">Build Name</label>
-                  </h3>
-                  <input
-                    id="suggested-build-name"
-                    className="input-field"
-                    placeholder="build name"
-                    name="build_name"
-                    value={buildForm.build_name}
-                    onChange={handleOnTextAreaChange}
-                  ></input>
-                  <h3>
-                    <label htmlFor="suggested-build-form">
-                      Build Description
-                    </label>
-                  </h3>
-                  <textarea
-                    id="suggested-build-form"
-                    className="input-field"
-                    placeholder="description..."
-                    name="build_description"
-                    value={buildForm.build_description}
-                    onChange={handleOnTextAreaChange}
-                  ></textarea>
-                </form>
-              </div>
-            ) : (
-              ""
+
+            {user.user_type == "Customer" && (
+              <label htmlFor="checkbox1">
+                <input
+                  id="checkbox1"
+                  name="checkbox"
+                  type="checkbox"
+                  checked={isChecked}
+                  onClick={handleCheckBox}
+                />
+                Check this box if you'd like to share your build!
+              </label>
             )}
+
+            {user.user_type == ("Owner" || "Employee") ||
+              (isChecked && (
+                <div className="build-description-container">
+                  <form>
+                    <h3>
+                      <label htmlFor="suggested-build-name">Build Name</label>
+                    </h3>
+                    <input
+                      id="suggested-build-name"
+                      className="input-field"
+                      placeholder="build name"
+                      name="build_name"
+                      value={buildForm.build_name}
+                      onChange={handleOnTextAreaChange}
+                    ></input>
+                    <h3>
+                      <label htmlFor="suggested-build-form">
+                        Build Description
+                      </label>
+                    </h3>
+                    <textarea
+                      id="suggested-build-form"
+                      className="input-field"
+                      placeholder="description..."
+                      name="build_description"
+                      value={buildForm.build_description}
+                      onChange={handleOnTextAreaChange}
+                    ></textarea>
+                  </form>
+                </div>
+              ))}
             <div className="buttons-container">
               <div className="buttons-container__save-options">
                 <Button className="blue-primary">Validate Build</Button>
@@ -108,7 +142,7 @@ export default function MyBuild() {
                   Discard Build
                 </Button>
               </div>
-              {user.user_type == ("Owner" || "Employee") ? (
+              {user.user_type == ("Owner" || "Employee") || isChecked ? (
                 <Button
                   className="buttons-container__submit-build black-primary"
                   onClick={handleSuggestBuild}
